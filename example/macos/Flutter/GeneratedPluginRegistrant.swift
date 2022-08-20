5e45f587a9537e2879cb6ec664302c7d2919e3be
Dominik Roszkowski <dominik@roszkowski.dev>
2022-08-20 23:38:17 +0200

    chore: init plugin

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import native_view
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  NativeViewPlugin.register(with: registry.registrar(forPlugin: "NativeViewPlugin"))
+}
